9f57a0deb599527d83a1aa8c99037df5154503f4
nab138 <56705177+nab138@users.noreply.github.com>
2024-04-03 20:14:09 -0400

    Add javadoc

--- Pathfinding/src/main/java/me/nabdev/pathfinding/utilities/DriverStationWrapper.java
+++ Pathfinding/src/main/java/me/nabdev/pathfinding/utilities/DriverStationWrapper.java
@@ -5,12 +5,23 @@ import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 import java.util.Optional;
 
+/**
+ * A wrapper class for the DriverStation class that allows for the use of the
+ * DriverStation class in an environment where wpiHalJNI is unavailable (I.E.
+ * OxplorerGUI). Returns default values if -Dnohaljni=true is given to the JVM.
+ */
 public class DriverStationWrapper {
     private static boolean canUseJni;
     static {
         canUseJni = !Boolean.getBoolean("nohaljni");
     }
 
+    /**
+     * Returns whether the Driver Station is currently enabled. False if HALJNI is
+     * disabled.
+     * 
+     * @return true if the Driver Station is currently enabled, false otherwise
+     */
     public static boolean isAutonomous() {
         if (canUseJni) {
             return DriverStation.isAutonomous();
@@ -19,6 +30,12 @@ public class DriverStationWrapper {
         }
     }
 
+    /**
+     * Returns whether the Driver Station is currently enabled. True if HALJNI is
+     * disabled.
+     * 
+     * @return true if the Driver Station is currently enabled, false otherwise
+     */
     public static boolean isTeleop() {
         if (canUseJni) {
             return DriverStation.isTeleop();
@@ -27,6 +44,12 @@ public class DriverStationWrapper {
         }
     }
 
+    /**
+     * Returns whether the Driver Station is currently enabled. False if HALJNI is
+     * disabled.
+     * 
+     * @return true if the Driver Station is currently enabled, false otherwise
+     */
     public static double getMatchTime() {
         if (canUseJni) {
             return DriverStation.getMatchTime();
@@ -35,6 +58,12 @@ public class DriverStationWrapper {
         }
     }
 
+    /**
+     * Returns the alliance that the driver station is on. If HALJNI is disabled,
+     * returns an empty Optional.
+     * 
+     * @return the alliance that the driver station is on
+     */
     public static Optional<Alliance> getAlliance() {
         if (canUseJni) {
             return DriverStation.getAlliance();
